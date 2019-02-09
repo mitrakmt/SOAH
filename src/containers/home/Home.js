72aa61b582e09293d7f8a9466d393e9af3dae576
Michael Mitrakos <mike.mitrakos@gmail.com>
2019-02-09 16:36:56 -0600

    feat/header: desktop header completed. Integration work needed upon completion of home page.

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 
-import Header from 'components/header/header'
-
 import './home.scss'
 
 class Home extends Component {
@@ -9,7 +7,6 @@ class Home extends Component {
     return (
       <div className="homeContainer" id="home">
         <h1>Home</h1>
-        <Header />
       </div>
     )
   }
