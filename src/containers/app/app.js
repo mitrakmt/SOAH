86059a77428947e01b6c9d0c4e8fa12cad847ef5
Michael Mitrakos <mike.mitrakos@gmail.com>
2019-02-09 13:32:16 -0600

    initial project

---
+++ src/containers/app/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import './app.scss'
+import '../../styles/fonts.scss'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app" id="root">
+        <h1>Home</h1>
+      </div>
+    )
+  }
+}
+
+export default App
