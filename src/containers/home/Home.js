b3ef388e6efe7d3246392781dc5d964b02b9a6a7
Michael Mitrakos <mike.mitrakos@gmail.com>
2019-02-10 21:14:55 -0600

    feat/press: letter from the producer completed

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 
 import './home.scss'
+import LetterFromTheDirector from '../../components/letter-from-the-director/letterFromTheDirector'
 
 class Home extends Component {
   render() {
     return (
       <div className="homeContainer" id="home">
         <h1>Home</h1>
+        <LetterFromTheDirector />
       </div>
     )
   }
