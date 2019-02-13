e8cdf9717b3b509d7386bc7c4d6225df66fb4662
Michael Mitrakos <mike.mitrakos@gmail.com>
2019-02-13 00:03:36 -0600

    feat/navigation: navigation dropdown complete besides functionality

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import Navigation from 'components/navigation/navigation'
+
 import './home.scss'
 
 class Home extends Component {
@@ -7,6 +9,7 @@ class Home extends Component {
     return (
       <div className="homeContainer" id="home">
         <h1>Home</h1>
+        <Navigation />
       </div>
     )
   }
