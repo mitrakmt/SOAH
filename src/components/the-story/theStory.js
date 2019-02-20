5285e74e055893ced2c913b869ff69509430f576
Michael Mitrakos <mike.mitrakos@gmail.com>
2019-02-19 23:54:04 -0600

    feat: the story view (#10)
    * feat/theStory: the story desktop view copmlete
    
    * feat/theStory: mobile responsiveness complete
    
    * feat/theStory: fix merge conflict and use html escape for apostraphe

---
+++ src/components/the-story/theStory.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+import BackgroundImage from 'images/SOAH_Still99.jpg'
+import Quote from 'components/quote/quote'
+
+import './theStory.scss'
+
+class TheStory extends Component {
+  render() {
+    return (
+      <div className="theStory">
+        <img
+          src={BackgroundImage}
+          alt="background"
+          className="theStory-background"
+        />
+        <div className="theStory-header">
+          <span className="theStory-header-line" />
+          <h3 className="theStory-header-text">The Story</h3>
+        </div>
+        <div className="theStory-description">
+          <Quote />
+          <p className="theStory-description-text">
+            Sides of a Horn is the first film to tell the story of Africa&#39;s
+            poaching war from both sides of the fence. Based on actual events,
+            and filmed in the townships and game reserves most directly impacted
+            by wildlife crime, this dramatic short film paints an unbiased
+            portrait of a modern war that is tearing communities apart and
+            driving a pre-historic species to the verge of extinction.
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TheStory
