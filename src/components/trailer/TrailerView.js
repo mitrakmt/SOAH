871b6929e127c61afc7ba130979dfbd8907dec27
Michael Flores <mike@michaelflores.io>
2019-02-20 20:33:30 -0800

    feat: content and style updates

--- src/components/trailer/TrailerView.js
+++ src/components/trailer/TrailerView.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import trailerThumbnail from '../../images/trailerThumbnail.jpeg'
+import Facebook from 'components/social/facebook'
+import Twitter from 'components/social/twitter'
+import Instagram from 'components/social/instagram'
+import Imdb from 'components/social/imdb'
 import './trailerView.scss'
 
 class TrailerView extends Component {
@@ -8,6 +12,52 @@ class TrailerView extends Component {
     return (
       <div className={`${sectionName}-view`}>
         <img className="thumbnail" alt="Sides of a Horn trailer thumbnail" src={trailerThumbnail} />
+        <div className="footer">
+          <div className="footer-section">
+            <div className="cta-container">
+              <h3>spread the word.</h3>
+              <h3>share the trailer:</h3>
+            </div>
+            <div className="socialGroup-container">
+              <div className="socialGroup-line">
+                <Instagram
+                  href="https://www.instagram.com/sidesofahorn"
+                  width="15px"
+                  height="15px"
+                  fill="white"
+                />
+                <Twitter
+                  href="https://twitter.com/sidesofahorn"
+                  width="15px"
+                  height="15px"
+                  fill="white"
+                />
+                <Facebook
+                  href="https://www.facebook.com/sidesofahorn/"
+                  width="15px"
+                  height="15px"
+                  fill="white"
+                />
+              </div>
+              <div className="socialGroup-line">
+                <Imdb
+                  href="https://www.imdb.com/title/tt7434998/"
+                  width="15px"
+                  height="15px"
+                  fill="white"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="footer-section">
+            <h3>the short film will be released ON world ENVIRONMENT</h3>
+            <h3>DAY on JUNE 5, 2019, let the countdown begin:</h3>
+          </div>
+          <div className="footer-section">
+            <h3>get the</h3>
+            <h3>latest:</h3>
+          </div>
+        </div>
       </div>
     )
   }
