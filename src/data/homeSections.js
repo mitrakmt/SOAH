865c17a1da6ac0a3ec97138207041b7ed157b6b2
Michael Mitrakos <mike.mitrakos@gmail.com>
2019-02-21 19:39:33 -0600

    feat/integration: progress

--- src/data/homeSections.js
+++ src/data/homeSections.js
@@ -1,12 +1,30 @@
 import { HomeSection } from '../containers/home'
 import LandingPageView from '../components/landingPage'
+import TheStory from 'components/the-story/theStory'
+// import LookInside from 'components/look-inside/lookInside'
+// import TakeAction from 'components/take-action/takeAction'
+import Credits from 'components/credits/credits'
 
 export default [
   {
     Container: HomeSection,
     props: {
       Component: LandingPageView,
-      sectionName: 'landingPage'
-    }
-  }
+      sectionName: 'landingPage',
+    },
+  },
+  {
+    Container: HomeSection,
+    props: {
+      Component: TheStory,
+      sectionName: 'theStory',
+    },
+  },
+  {
+    Container: HomeSection,
+    props: {
+      Component: Credits,
+      sectionName: 'credits',
+    },
+  },
 ]
