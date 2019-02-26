a7cb45861a78c3b22045cbf72370af9d9e54c4ba
Michael Flores <mike@michaelflores.io>
2019-02-25 21:46:01 -0800

    feat: update flip clock to countdownt to movie release date
    Currently in days, hours, and minutes

--- src/components/flipClock/FlipClock.js
+++ src/components/flipClock/FlipClock.js
@@ -19,7 +19,7 @@ class FlipClock extends Component {
 	componentDidMount() {
 		this.timerID = setInterval(
 			() => this.updateTime(),
-			50
+			1000
 		);
 	}
   
@@ -28,33 +28,55 @@ class FlipClock extends Component {
 	}
   
 	updateTime() {
+    const dateFuture = new Date("Jun 5, 2019 00:00:00").getTime();
 		// get new date
-		const time = new Date();
+    const time = new Date();
+    // get total seconds between the times
+    let delta = Math.abs(dateFuture - time) / 1000;
+
+    // calculate (and subtract) whole days
+    const days = Math.floor(delta / 86400);
+    delta -= days * 86400;
+
+    // calculate (and subtract) whole hours
+    const hours = Math.floor(delta / 3600) % 24;
+    delta -= hours * 3600;
+
+    // calculate (and subtract) whole minutes
+    const minutes = Math.floor(delta / 60) % 60;
+    delta -= minutes * 60;
+
+    // what's left is seconds
+    // currently unused
+    const seconds = delta % 60;  // in theory the modulus is not required
 		// set time units
-		const hours = time.getHours();
-		const minutes = time.getMinutes();
-		const seconds = time.getSeconds();
-		// on hour chanage, update hours and shuffle state
-		if( hours !== this.state.hours) {
-			const hoursShuffle = !this.state.hoursShuffle;
+		// const hours = time.getHours();
+		// const minutes = time.getMinutes();
+		// const seconds = time.getSeconds();
+		// on days chanage, update dayss and shuffle state
+		if( days !== this.state.hours) {
+      // Note this usage of days in the hours field is a quirk of combining the old clock functionality with the countdown
+      const hoursShuffle = !this.state.hoursShuffle;
 			this.setState({
-				hours,
+				hours: days,
 				hoursShuffle
 			});
 		}
-		// on minute chanage, update minutes and shuffle state
-		if( minutes !== this.state.minutes) {
-			const minutesShuffle = !this.state.minutesShuffle;
+		// on hours chanage, update hours and shuffle state
+		if( hours !== this.state.minutes) {
+      const minutesShuffle = !this.state.minutesShuffle;
+      // Note this usage of hours in the minutes field is a quirk of combining the old clock functionality with the countdown
 			this.setState({
-				minutes,
+				minutes: hours,
 				minutesShuffle
 			});
 		}
-		// on second chanage, update seconds and shuffle state
-		if( seconds !== this.state.seconds) {
-			const secondsShuffle = !this.state.secondsShuffle;
+		// on minute chanage, update minutes and shuffle state
+		if( minutes !== this.state.seconds) {
+      const secondsShuffle = !this.state.secondsShuffle;
+      // Note this usage of minutes in the seconds field is a quirk of combining the old clock functionality with the countdown
 			this.setState({
-				seconds,
+				seconds: minutes,
 				secondsShuffle
 			});
 		}
