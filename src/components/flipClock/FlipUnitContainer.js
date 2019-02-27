ffed5f7786cc7d8b8e9de0e63ade8fc6928c7941
Michael Mitrakos <mike.mitrakos@gmail.com>
2019-02-26 20:25:44 -0600

    feat: complete (#47)

--- src/components/flipClock/FlipUnitContainer.js
+++ src/components/flipClock/FlipUnitContainer.js
@@ -2,68 +2,42 @@ import React from 'react'
 import StaticCard from './StaticCard'
 import AnimatedCard from './AnimatedCard'
 
-const FlipUnitContainer = ({ digit, shuffle, unit }) => {	
+const FlipUnitContainer = ({ digit, shuffle, unit }) => {
   // assign digit values
-  let currentDigit = digit;
-  let previousDigit = digit - 1;
+  let currentDigit = digit
+  let previousDigit = digit - 1
 
   // to prevent a negative value
-  if ( unit !== 'hours') {
-    previousDigit = previousDigit === -1 
-      ? 59 
-      : previousDigit;
+  if (unit !== 'hours') {
+    previousDigit = previousDigit === -1 ? 59 : previousDigit
   } else {
-    previousDigit = previousDigit === -1 
-      ? 23 
-      : previousDigit;
+    previousDigit = previousDigit === -1 ? 23 : previousDigit
   }
 
   // add zero
-  if ( currentDigit < 10 ) {
-    currentDigit = `0${currentDigit}`;
-  } 
-  if ( previousDigit < 10 ) {
-    previousDigit = `0${previousDigit}`;
+  if (currentDigit < 10) {
+    currentDigit = `0${currentDigit}`
+  }
+  if (previousDigit < 10) {
+    previousDigit = `0${previousDigit}`
   }
 
   // shuffle digits
-  const digit1 = shuffle 
-    ? previousDigit 
-    : currentDigit;
-  const digit2 = !shuffle 
-    ? previousDigit 
-    : currentDigit;
+  const digit1 = shuffle ? previousDigit : currentDigit
+  const digit2 = !shuffle ? previousDigit : currentDigit
 
   // shuffle animations
-  const animation1 = shuffle 
-    ? 'fold' 
-    : 'unfold';
-  const animation2 = !shuffle 
-    ? 'fold' 
-    : 'unfold';
+  const animation1 = shuffle ? 'fold' : 'unfold'
+  const animation2 = !shuffle ? 'fold' : 'unfold'
 
-  return(
+  return (
     <div className={'flipUnitContainer'}>
-      <StaticCard 
-        position={'upperCard'} 
-        digit={currentDigit} 
-        />
-      <StaticCard 
-        position={'lowerCard'} 
-        digit={previousDigit} 
-        />
-      <AnimatedCard 
-        position={'first'}
-        digit={digit1}
-        animation={animation1}
-        />
-      <AnimatedCard 
-        position={'second'}
-        digit={digit2}
-        animation={animation2}
-        />
+      <StaticCard position={'upperCard'} digit={currentDigit} />
+      <StaticCard position={'lowerCard'} digit={previousDigit} />
+      <AnimatedCard position={'first'} digit={digit1} animation={animation1} />
+      <AnimatedCard position={'second'} digit={digit2} animation={animation2} />
     </div>
-  );
-};
+  )
+}
 
 export default FlipUnitContainer
