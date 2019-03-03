902db6301237ff4fe66a75aa5dd2b59e9f36e8a4
Michael Flores <mike@michaelflores.io>
2019-03-02 23:20:54 -0800

    fix: disable smooth scroll (#57)

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -31,12 +31,12 @@ class Navigation extends Component {
     const elementFound = document.getElementById(view)
 
     if (isHomeView && elementFound) {
-      elementFound.scrollIntoView({ behavior: 'smooth' })
+      elementFound.scrollIntoView()
     } else {
       this.props.history.push(`/${view}`)
       if (isHomeView) {
         setTimeout(() => {
-          document.getElementById(view).scrollIntoView({ behavior: 'smooth' })
+          document.getElementById(view).scrollIntoView()
         }, 250)
       }
     }
