5ca3fc889c9db020d37188a4cdeffe9d2cfcc739
Michael Mitrakos <mike.mitrakos@gmail.com>
2019-03-23 19:05:43 -0500

    fix/askedChanged1: multiple fixes completed (#74)

--- src/components/takeAction/takeAction.js
+++ src/components/takeAction/takeAction.js
@@ -7,6 +7,7 @@ import RhinoImage from 'images/SOAH_Still2.png'
 import ManImage from 'images/SOAH_Still5.png'
 import MenImage from 'images/SOAH_Still29.png'
 import BabyImage from 'images/SOAH_Still33.png'
+import BackgroundImage from 'images/SOAH_Still46.png'
 
 import './takeAction.scss'
 
@@ -20,6 +21,11 @@ class TakeActionView extends Component {
 
     return (
       <div className={`${sectionName}`}>
+        <img
+          src={BackgroundImage}
+          className={`${sectionName}-background`}
+          alt="take action background"
+        />
         <div className={`${sectionName}-header`}>
           <h3 className={`${sectionName}-header-text`}>Take Action</h3>
           <ScrollAnimation
