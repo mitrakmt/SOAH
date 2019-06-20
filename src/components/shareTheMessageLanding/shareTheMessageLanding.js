ce91e39c95d16f1723b39fba228b643ef194e5f6
Michael Mitrakos <mike.mitrakos@gmail.com>
2019-06-20 00:11:21 -0500

    hotfix: optimize share the message background image

--- src/components/shareTheMessageLanding/shareTheMessageLanding.js
+++ src/components/shareTheMessageLanding/shareTheMessageLanding.js
@@ -9,7 +9,7 @@ import LinkedIn from 'components/social/linkedin'
 import Email from 'components/social/email'
 import Tumblr from 'components/social/tumblr'
 
-import HeaderImage from 'images/SOAH_Still5_long.png'
+import HeaderImage from 'images/SOAH_Still5_long.jpg'
 
 import './shareTheMessageLanding.scss'
 
