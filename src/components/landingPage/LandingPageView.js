753be79271c74f2f29ce23144bcff003cd294697
Michael Flores <mike@michaelflores.io>
2019-06-20 19:09:22 -0700

    feat: add watch film cta

--- src/components/landingPage/LandingPageView.js
+++ src/components/landingPage/LandingPageView.js
@@ -3,6 +3,7 @@ import awfLogo from '../../images/awf.svg'
 import Header from '../header'
 import SoahLogo from '../../images/SOAH_logo.png'
 import ScrollAnimation from 'react-animate-on-scroll'
+import { homeSections } from 'data'
 
 import AnimationLogo from 'images/redLogo.png'
 import AnimationBackground from 'images/SOAH_Still46.png?sizes[]=512,sizes[]=1024'
@@ -10,6 +11,32 @@ import AnimationBackground from 'images/SOAH_Still46.png?sizes[]=512,sizes[]=102
 import './landingPageView.scss'
 
 class LandingPageView extends Component {
+  navigateToView(view) {
+    const isHomeView = homeSections
+    .map(({ props: { sectionName } }) => sectionName)
+    .includes(view)
+  // Using view as an ID when it's a home section
+  const elementFound = document.getElementById(view)
+
+  if (isHomeView && elementFound) {
+    elementFound.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'start',
+    })
+  } else {
+    this.props.history.push(`/${view}`)
+    if (isHomeView) {
+      setTimeout(() => {
+        document.getElementById(view).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+          inline: 'start',
+        })
+      }, 250)
+    }
+  }
+  }
   render() {
     const { sectionName } = this.props
     return (
@@ -32,6 +59,12 @@ class LandingPageView extends Component {
         <div className="header-container">
           <Header />
         </div>
+        <h3
+          className="watchFilmCTA"
+          onClick={() => this.navigateToView('trailer')}
+        >
+          Watch Film
+        </h3>
         <div className="association-container">
           <p className="association-text">in association with</p>
           <img alt="Africa Wildlife Foundation logo" src={awfLogo} />
